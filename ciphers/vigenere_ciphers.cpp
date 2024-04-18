e3c26418be0acee40b0017c0185ed3f43729be0e
Moncwari <easidorov@edu.hse.ru>
2024-04-18 18:43:14 +0300

    Vigenere ciphers V1.0 with tests and documentatin

---
+++ ciphers/vigenere_ciphers.cpp
@@ -0,0 +1,244 @@
+#include "vigenere_ciphers.hpp"
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <exception>
+
+/**
+ * \file vigenere_ciphers.cpp
+ * 
+ * In that file we describe the functions of 3 types of vigenere cipherings and decipherings, namely short key gamma, self key from opentext and self key from ciphertext, and the validation function for them.
+*/
+
+
+
+/**
+ * The validation function for Vigenere ciphers.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \throw input_data_invalid with the suitable message in case of wrong alphabet, wrong key or wrong text.
+*/
+void vigenere_data_is_valid(std::string const &text, std::string const &key, std::string const &alphabet)
+{
+    for (char c : alphabet)
+    {
+        if (count(alphabet.begin(), alphabet.end(), c) > 1)
+        {
+            throw input_data_invalid("Alphabet letter repeat error");
+        }
+    }
+    for (char c : key)
+    {
+        if (count(alphabet.begin(), alphabet.end(), c) < 1)
+        {
+            throw input_data_invalid("Unknown key symbol");
+        }
+    }
+    long long coun{0};
+    for (char c : alphabet)
+    {
+        coun += count(text.begin(), text.end(), c);
+    }
+    if (coun < text.size())
+    {
+        throw input_data_invalid("Unknown text symbol");
+    }
+}
+
+/**
+ * The function of Vigenere ciphering with gamma formed with repeating short key.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \return ciphertext for that input data if data is correct.
+ * \return error message if data is incorrect.
+*/
+std::string repeat_key_ciphering(std::string const &text, std::string const &key, std::string const &alphabet)
+{
+    try{
+        vigenere_data_is_valid(text, key, alphabet);
+        std::string cipher{""};
+        long long len_alph{alphabet.size()};
+        for (int i = 0; i < text.size(); i++)
+        {
+            int point = std::find(alphabet.begin(), alphabet.end(), text[i]) - alphabet.begin();
+            int gam = std::find(alphabet.begin(), alphabet.end(), key[i % key.size()]) - alphabet.begin();
+            cipher += alphabet[(point + gam) % len_alph];
+        }
+        return cipher;
+
+    }
+    catch (input_data_invalid error)
+    {
+        return error.message;
+    }   
+}
+
+
+/**
+ * The function of Vigenere ciphering with gamma formed with repeating short key.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \return ciphertext for that input data if data is correct.
+ * \return error message if data is incorrect.
+*/
+std::string repeat_key_deciphering(std::string const &ciphertext, std::string const &key, std::string const &alphabet)
+{
+    try
+    {
+        vigenere_data_is_valid(ciphertext, key, alphabet);
+        std::string decipher{""};
+        long long len_alph{alphabet.size()};
+        for (int i = 0; i < ciphertext.size(); i++)
+        {
+            int point = std::find(alphabet.begin(), alphabet.end(), ciphertext[i]) - alphabet.begin();
+            int gam = std::find(alphabet.begin(), alphabet.end(), key[i % (key.size())]) - alphabet.begin();
+            decipher += alphabet[((point - gam) % len_alph + len_alph) % len_alph];
+        }
+        return decipher;
+    }
+    catch(input_data_invalid error)
+    {
+        return error.message;
+    }
+}
+
+
+/**
+ * The function of Vigenere ciphering with gamma formed with selfkey from opentext.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \return ciphertext for that input data if data is correct.
+ * \return error message if data is incorrect.
+*/
+std::string self_open_key_ciphering(std::string const &text, std::string const &key, std::string const &alphabet)
+{
+    try
+    {
+        vigenere_data_is_valid(text, key, alphabet);
+        std::string cipher{""};
+        unsigned long long len_alph{alphabet.size()};
+        std::string gam = key + text;
+        for (int i = 0; i < text.size(); i++)
+        {
+            int first_point = std::find(alphabet.begin(), alphabet.end(), text[i]) - alphabet.begin();
+            int sec_point = std::find(alphabet.begin(), alphabet.end(), gam[i]) - alphabet.begin();
+            cipher += alphabet[(first_point + sec_point) % len_alph];
+        }
+        return cipher;
+    }
+    catch(input_data_invalid error)
+    {
+        return error.message;
+    }
+}
+
+/**
+ * The function of Vigenere deciphering with gamma formed with selfkey from with opentext.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \return ciphertext for that input data if data is correct.
+ * \return error message if data is incorrect.
+*/
+std::string self_open_key_deciphering(std::string const &ciphertext, std::string const &key, std::string const &alphabet)
+{
+    try
+    {
+        vigenere_data_is_valid(ciphertext, key, alphabet);
+        std::string decipher{""};
+        long long len_alph{alphabet.size()};
+        std::string gam = key;
+        for (int i = 0; i < ciphertext.size(); i++)
+        {
+            int first_point = std::find(alphabet.begin(), alphabet.end(), ciphertext[i]) - alphabet.begin();
+            int sec_point = std::find(alphabet.begin(), alphabet.end(), gam[i]) - alphabet.begin();
+            decipher += alphabet[((first_point - sec_point) % len_alph + len_alph) % len_alph];
+            gam += alphabet[((first_point - sec_point) % len_alph + len_alph) % len_alph];
+        }
+        return decipher;
+    }
+    catch(input_data_invalid error)
+    {
+        return error.message;
+    }
+}
+
+/**
+ * The function of Vigenere ciphering with gamma formed with selfkey from with ciphertext.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \return ciphertext for that input data if data is correct.
+ * \return error message if data is incorrect.
+*/
+std::string self_cipher_key_ciphering(std::string const &text, std::string const &key, std::string const &alphabet)
+{
+    try
+    {
+        vigenere_data_is_valid(text, key, alphabet);
+        std::string cipher{""};
+        unsigned long long len_alph{alphabet.size()};
+        std::string gam = key;
+        for (int i = 0; i < text.size(); i++)
+        {
+            int first_point = std::find(alphabet.begin(), alphabet.end(), text[i]) - alphabet.begin();
+            int sec_point = std::find(alphabet.begin(), alphabet.end(), gam[i]) - alphabet.begin();
+            cipher += alphabet[(first_point + sec_point) % len_alph];
+            gam += alphabet[(first_point + sec_point) % len_alph];
+        }
+        return cipher;
+    }
+    catch(input_data_invalid error)
+    {
+        return error.message;
+    }
+}
+
+/**
+ * The function of Vigenere deciphering with gamma formed with selfkey from the ciphertext.
+ * 
+ * \param[in] text The text we want to encrypt or decrypt with some of Vigenere ciphers.
+ * \param[in] key The key for Vigenere ciphering.
+ * \param[in] alphabet The alphabet of Vigenere ciphering.
+ * 
+ * \return ciphertext for that input data if data is correct.
+ * \return error message if data is incorrect.
+*/
+std::string self_cipher_key_deciphering(std::string const &ciphertext, std::string const &key, std::string const &alphabet)
+{
+    try
+    {
+        vigenere_data_is_valid(ciphertext, key, alphabet);
+        std::string decipher{""};
+        long long len_alph{alphabet.size()};
+        std::string gam = key + ciphertext;
+        for (int i = 0; i < ciphertext.size(); i++)
+        {
+            int first_point = std::find(alphabet.begin(), alphabet.end(), ciphertext[i]) - alphabet.begin();
+            int sec_point = std::find(alphabet.begin(), alphabet.end(), gam[i]) - alphabet.begin();
+            decipher += alphabet[(first_point - sec_point + len_alph) % len_alph];
+        }
+        return decipher;
+    }
+    catch(input_data_invalid error)
+    {
+        return error.message;
+    }
+}
---
+++ ciphers/vigenere_ciphers.hpp
@@ -0,0 +1,39 @@
+#ifndef VIGENERE_CIPHERS_HPP
+#define VIGENERE_CIPHERS_HPP
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class exception
+{
+    public:
+        exception() noexcept;
+        exception(const exception&) noexcept;
+        exception& operator=(const exception&) noexcept;
+        virtual ~exception();
+    virtual const char* what() const noexcept;
+};
+
+class input_data_invalid: public std::exception
+{
+    public:
+        std::string message;
+        input_data_invalid(const std::string& _message){
+        message = _message;
+        }
+    const char* what() const noexcept override
+    {
+        return message.c_str();
+    }
+};
+
+void vigenere_data_is_valid(std::string const &text, std::string const &key, std::string const &alphabet);
+std::string repeat_key_ciphering(std::string const &text, std::string const &key, std::string const &alphabet);
+std::string repeat_key_deciphering(std::string const &ciphertext, std::string const &key, std::string const &alphabet);
+std::string self_open_key_ciphering(std::string const &text, std::string const &key, std::string const &alphabet);
+std::string self_open_key_deciphering(std::string const &ciphertext, std::string const &key, std::string const &alphabet);
+std::string self_cipher_key_ciphering(std::string const &text, std::string const &key, std::string const &alphabet);
+std::string self_cipher_key_deciphering(std::string const &ciphertext, std::string const &key, std::string const &alphabet);
+
+#endif
