f12b972592c7a48090b9aaccaa6bfe2cce445a5f
Moncwari <easidorov@edu.hse.ru>
2024-06-14 17:31:05 +0300

    Major changes

--- vigenere_ciphers/vigenere_ciphers.hpp
+++ vigenere_ciphers/vigenere_ciphers.hpp
@@ -4,30 +4,7 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
-class exception
-{
-public:
-    exception() noexcept;
-    exception(const exception &) noexcept;
-    exception &operator=(const exception &) noexcept;
-    virtual ~exception();
-    virtual const char *what() const noexcept;
-};
-
-class input_data_invalid : public std::exception
-{
-public:
-    std::string message;
-    input_data_invalid(const std::string &_message)
-    {
-        message = _message;
-    }
-    const char *what() const noexcept override
-    {
-        return message.c_str();
-    }
-};
+#include "exceptions.hpp"
 
 void vigenere_data_is_valid(const std::string &text, const std::string &key, const std::string &alphabet);
 std::string repeat_key_ciphering(const std::string &text, const std::string &key, const std::string &alphabet);
