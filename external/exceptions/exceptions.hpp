de5682e3d52e3768f98dbebc93817a6199900277
Moncwari <easidorov@edu.hse.ru>
2024-06-15 11:20:01 +0300

    LLVM format

--- external/exceptions/exceptions.hpp
+++ external/exceptions/exceptions.hpp
@@ -1,8 +1,8 @@
 #ifndef EXCEPTIONS_HPP
 #define EXCEPTIONS_HPP
 
-#include <string>
 #include <exception>
+#include <string>
 
 /**
  * \file exceptions.hpp
@@ -10,41 +10,34 @@
  * A file containing classes for working with exceptions.
  */
 
- /**
+/**
  * A class for working with exceptions.
  *
  */
-class exception
-{
+class exception {
 public:
-    exception() noexcept;
-    exception(const exception&) noexcept;
-    exception& operator=(const exception&) noexcept;
-    virtual ~exception();
-    virtual const char* what() const noexcept;
+  exception() noexcept;
+  exception(const exception &) noexcept;
+  exception &operator=(const exception &) noexcept;
+  virtual ~exception();
+  virtual const char *what() const noexcept;
 };
 
 /**
  * A class to simplify error output.
  *
  */
-class input_data_invalid: public std::exception
-{
+class input_data_invalid : public std::exception {
 public:
-    std::string message;
-    input_data_invalid(const std::string& _message){
-        message = _message;
-    }
+  std::string message;
+  input_data_invalid(const std::string &_message) { message = _message; }
 
-/**
- * The function that returns an error.
- *
- * \return message.c_str() error message.
- */
-    const char* what() const noexcept override
-    {
-        return message.c_str();
-    }
+  /**
+   * The function that returns an error.
+   *
+   * \return message.c_str() error message.
+   */
+  const char *what() const noexcept override { return message.c_str(); }
 };
 
 #endif // EXCEPTIONS
